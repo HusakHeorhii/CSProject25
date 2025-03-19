e2ef25898614a3666f5c281dc5027d9a05a9474c
mac <mac@macs-MacBook-Pro.local>
2025-03-19 14:35:10 +0000

    finished doing the index.html and its basic functionalities; todo: advanced

---
+++ report/script.js
@@ -0,0 +1,178 @@
+let month;
+
+document.getElementById("filter_submit").onclick = function() {
+    month = document.getElementById("month").value;
+    console.log(month);
+
+    if(month == '2024-01'){
+        document.getElementById("january").style.display = "block";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }
+    else if(month == '2024-02'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "block";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-03'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "block";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-04'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "block";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-05'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "block";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-06'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "block";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-07'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "block";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-08'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "block";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-09'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "block";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-10'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "block";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-11'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "block";
+        document.getElementById("december").style.display = "none";
+
+    }else if(month == '2024-12'){
+        document.getElementById("january").style.display = "none";
+        document.getElementById("february").style.display = "none";
+        document.getElementById("march").style.display = "none";
+        document.getElementById("april").style.display = "none";
+        document.getElementById("may").style.display = "none";
+        document.getElementById("june").style.display = "none";
+        document.getElementById("july").style.display = "none";
+        document.getElementById("august").style.display = "none";
+        document.getElementById("september").style.display = "none";
+        document.getElementById("october").style.display = "none";
+        document.getElementById("november").style.display = "none";
+        document.getElementById("december").style.display = "block";
+
+    }
+    
+}
